25d35b5b27b97a62cef4b56912ad000748345acd
MoxTY <omelette70@hotmail.com>
2019-08-05 16:02:31 +0800

    作用域与闭包————魔塔Demo

--- demo1/closure/index.js
+++ demo1/closure/index.js
@@ -9,7 +9,6 @@
 				img.src = src;
 			});
 		};
-
 		const context = document.getElementById('content').getContext('2d');
 		const heroImg = new Image();
 		const allSpriteImg = new Image();
@@ -17,7 +16,6 @@
 			imgTask(heroImg, './hero.png'),
 			imgTask(allSpriteImg, './all.jpg'),
 		]);
-
 		return {
 			/**
 			 * @param {Function} [callback] - 当准备好了之后要调用的回掉函数
@@ -29,11 +27,10 @@
 			}
 		};
 	}
-
 	// Action2
 	function drawHero(context, heroImg, allSpriteImg) {
 		var draw = function () {
-			this.context
+				this.context
 				.drawImage(
 					this.img,
 					this.imgPos.x,
@@ -46,6 +43,7 @@
 					this.rect.height
 				);
 		}
+
 		var clear = function () {
 			this.context
 				.clearRect(
@@ -96,51 +94,64 @@
 
 			draw: draw
 		};
-
 		hero.draw();
 		monster.draw();
-
-		console.log(context)
 		document.onkeydown = function (event) {
 			var e = event || window.event || arguments.callee.call.arguments[0];
 			switch (e && e.keyCode) {
 				case 40:
-					if(hero.rect.y != 260){
-						// console.log(hero.rect.y)
-						hero.clear();
-						hero.rect.y += 10;
-						hero.draw();
+					if(hero.rect.y != 260) {
+						if( hero.rect.y < 100 && hero.rect.y > 50 && hero.rect.x > 65 &&  hero.rect.x < 140 ){
+							confirm('与野怪相撞，发起攻击获得红包奖励')
+							return;
+						}else{
+							hero.clear();
+							hero.rect.y += 10;
+							hero.draw();
+						}
 					}
 					break;
 				case 37:
-					if(hero.rect.x != 0){
-						hero.clear();
-						hero.rect.x -= 10;
-						hero.draw();
+					if(hero.rect.x != 0) {
+						if( hero.rect.x > 60 && hero.rect.x < 150 && hero.rect.y > 60 && hero.rect.y < 140){
+							confirm('与野怪相撞，发起攻击获得红包奖励')
+							return;
+						}else{
+							hero.clear();
+							hero.rect.x -= 10;
+							hero.draw();
+						}
 					}
 					break;
 				case 39:
-					if(hero.rect.x != 460){
-						console.log(hero.rect.x)
-						hero.clear();
-						hero.rect.x += 10;
-						hero.draw();
+					if(hero.rect.x != 460) {
+						if( hero.rect.x < 140 && hero.rect.x > 50 && hero.rect.y > 60 && hero.rect.y < 140){
+							confirm('与野怪相撞，发起攻击获得红包奖励')
+							return;
+						}else{
+							hero.clear();
+							hero.rect.x += 10;
+							hero.draw();
+						}
 					}
 					break;
 				case 38:
-					if(hero.rect.y != 0){
-						hero.clear();
-						hero.rect.y -= 10;
-						hero.draw();
+					if(hero.rect.y != 0) {
+						if( hero.rect.y > 100 && hero.rect.y < 150 && hero.rect.x > 65 &&  hero.rect.x < 140 ){
+							confirm(`与野怪相撞! 发起攻击获得红包奖励`)
+							return;
+						}else{
+							hero.clear();
+							hero.rect.y -= 10;
+							hero.draw();
+						}
 					}
 					break;
 				default:
 					break;
 			}
-
 		}
 	}
-
 	var resourceManager = prepare();
 	resourceManager.getResource(function (context, heroImg, allSpriteImg) {
 		drawHero(context, heroImg, allSpriteImg);
