e66a979e0ebfead1da7befffd126566553b1991c
MoxTY <omelette70@hotmail.com>
2019-08-05 14:18:21 +0800

    demo1_魔塔

---
+++ demo1/closure/index.js
@@ -0,0 +1,148 @@
+
+(function () {
+	// Action1
+	function prepare() {
+		const imgTask = (img, src) => {
+			return new Promise(function (resolve, reject) {
+				img.onload = resolve;
+				img.onerror = reject;
+				img.src = src;
+			});
+		};
+
+		const context = document.getElementById('content').getContext('2d');
+		const heroImg = new Image();
+		const allSpriteImg = new Image();
+		const allresourceTask = Promise.all([
+			imgTask(heroImg, './hero.png'),
+			imgTask(allSpriteImg, './all.jpg'),
+		]);
+
+		return {
+			/**
+			 * @param {Function} [callback] - 当准备好了之后要调用的回掉函数
+			 */
+			getResource(callback) {
+				allresourceTask.then(function () {
+					callback && callback(context, heroImg, allSpriteImg);
+				});
+			}
+		};
+	}
+
+	// Action2
+	function drawHero(context, heroImg, allSpriteImg) {
+		var draw = function () {
+			this.context
+				.drawImage(
+					this.img,
+					this.imgPos.x,
+					this.imgPos.y,
+					this.imgPos.width,
+					this.imgPos.height,
+					this.rect.x,
+					this.rect.y,
+					this.rect.width,
+					this.rect.height
+				);
+		}
+		var clear = function () {
+			this.context
+				.clearRect(
+					this.rect.x,
+					this.rect.y,
+					this.rect.width,
+					this.rect.height
+				)
+		}
+
+		var hero = {
+			img: heroImg,
+			context: context,
+			imgPos: {
+				x: 0,
+				y: 0,
+				width: 32,
+				height: 32
+			},
+
+			rect: {
+				x: 0,
+				y: 0,
+				width: 40,
+				height: 40
+			},
+
+			draw: draw,
+			clear: clear
+		};
+
+		var monster = {
+			img: allSpriteImg,
+			context: context,
+			imgPos: {
+				x: 858,
+				y: 529,
+				width: 32,
+				height: 32
+			},
+
+			rect: {
+				x: 100,
+				y: 100,
+				width: 40,
+				height: 40
+			},
+
+			draw: draw
+		};
+
+		hero.draw();
+		monster.draw();
+
+		console.log(context)
+		document.onkeydown = function (event) {
+			var e = event || window.event || arguments.callee.call.arguments[0];
+			switch (e && e.keyCode) {
+				case 40:
+					if(hero.rect.y != 260){
+						// console.log(hero.rect.y)
+						hero.clear();
+						hero.rect.y += 10;
+						hero.draw();
+					}
+					break;
+				case 37:
+					if(hero.rect.x != 0){
+						hero.clear();
+						hero.rect.x -= 10;
+						hero.draw();
+					}
+					break;
+				case 39:
+					if(hero.rect.x != 460){
+						console.log(hero.rect.x)
+						hero.clear();
+						hero.rect.x += 10;
+						hero.draw();
+					}
+					break;
+				case 38:
+					if(hero.rect.y != 0){
+						hero.clear();
+						hero.rect.y -= 10;
+						hero.draw();
+					}
+					break;
+				default:
+					break;
+			}
+
+		}
+	}
+
+	var resourceManager = prepare();
+	resourceManager.getResource(function (context, heroImg, allSpriteImg) {
+		drawHero(context, heroImg, allSpriteImg);
+	});
+})();
